4a690b83099771b0e902989e0619461de6af75f6
infvortx <53519839+infvortx@users.noreply.github.com>
2024-09-08 05:04:22 +0300

    Bun !!!

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,15 @@
 //https://nitro.unjs.io/config
 
 export default defineNitroConfig({
+  preset: "bun",
+  node: true,
+  inlineDynamicImports: true,
+  serveStatic: "inline",
+  esbuild: {
+    options: {
+      target: "esnext",
+    },
+  },
   srcDir: "server",
   experimental: {
     websocket: true,
